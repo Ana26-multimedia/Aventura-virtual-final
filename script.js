0ef38a55e023381942d1a01ddb6054e76b1fc793
Ana26-multimedia <a.acunac@uniandes.edu.co>
2026-04-03 03:57:33 +0000

    cambios en el style

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-const cohete = document.getElementById("cohete");
+<script>
+document.addEventListener("DOMContentLoaded", () => {
+  //
+  const cohete = document.getElementById("cohete");
+  const boton = document.getElementById("launchBtn");
 
-cohete.addEventListener("click", () => {
-  cohete.classList.remove("despegar");
-  void cohete.offsetWidth; // reinicia animación
-  cohete.classList.add("despegar");
+  if (cohete) {
+    
+    cohete.addEventListener("click", () => {
+      cohete.classList.remove("despegar");
+      void cohete.offsetWidth; 
+      cohete.classList.add("despegar");
+    });
+  }
+
+  if (boton) {
+   
+    boton.addEventListener("click", () => {
+      cohete.classList.remove("despegar");
+      void cohete.offsetWidth;
+      cohete.classList.add("despegar");
+    });
+  }
+
+ 
+  const pajaro = document.getElementById("pajaro");
+
+  if (pajaro) {
+    pajaro.addEventListener("click", () => {
+      if (pajaro.classList.contains("volando")) return;
+
+    
+      pajaro.classList.add("volando");
+    });
+
+  
+    pajaro.addEventListener("animationend", () => {
+      window.location.href = "escena2.html";
+    });
+  }
 });
-
-const cohete = document.getElementById("cohete");
-const boton = document.getElementById("launchBtn");
-
-boton.addEventListener("click", () => {
-  cohete.classList.remove("despegar");
-  void cohete.offsetWidth;
-  cohete.classList.add("despegar");
-});
+</script>
